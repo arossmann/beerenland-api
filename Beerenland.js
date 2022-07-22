30d5660d5d397aa4fd5e246beef5f1b7059030de
Arne Rossmann (Work) <arossman@Sleipnir.fritz.box>
2022-07-22 09:31:43 +0200

    improved script

--- Beerenland.js
+++ Beerenland.js
@@ -1,47 +1,64 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: shopping-basket;
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: magic;
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 
-const location = "Wolkersdorf"
+//const location = "Wolkersdorf"
+let location = "Wolkersdorf";
+const color = "0066CC"
+const title = "Berries in "+location
 const url = `https://arossmann.github.io/beerenland-api/result.json`
 const req = new Request(url)
 const res = await req.loadJSON() 
-// console.log(res)
 const loc = res.pick_conditions[0]
-// console.log(loc)
 
+const widget = new ListWidget()
+await createWidget()
 
-if (config.runsInWidget) {
-  // create and show widget
-  let widget = createWidget(`berry picking state in ${location}`, "#0066CC")
-  Script.setWidget(widget)
-  Script.complete()
-} else {
-  let table = new UITable()
-  let row = new UITableRow()
-  row.isHeader = true
-  row.addText(`berry picking state in ${location}`)
-  table.addRow(row)
-  for (let [key,value] of Object.entries(loc[location])){
-	console.log(value.berry+" "+value.status)
-    table.addRow(createRow(value.berry, value.status))
-  }
- table.present()
-}
-function createRow(title, image) {
-  let row = new UITableRow()
-  row.addText(title)
-  row.addImageAtURL(image)
-  return row
+try {
+  location=readParams();
+  location.toString();
+} catch(err){
+    location = "Wolkersdorf";
 }
 
-function createWidget(title, color) {
-  let w = new ListWidget()
-  w.backgroundColor = new Color(color)
-  let titleTxt = w.addText(title)
+// used for debugging if script runs inside the app
+if (!config.runsInWidget) {
+    await widget.presentSmall()
+}
+Script.setWidget(widget)
+Script.complete()
+
+function statusImage(status) {
+    let symbols;
+    symbols = {
+        "good" : function(){ return "🟢"; },
+        "bad" : function() { return "🟠"; },
+        "sale" : function() { return "⚫️"; },
+        "closed" : function() { return "🔴"; }
+    }
+    return symbols[status]();
+}
+
+
+// build the content of the widget
+async function createWidget() {
+  widget.backgroundColor = new Color(color)
+  widget.setPadding(10,10,10,10)
+  let titleTxt = widget.addText(title)
   titleTxt.textColor = Color.white()
-  titleTxt.font = Font.systemFont(12)
-  return w
+  titleTxt.font = Font.boldMonospacedSystemFont(12)
+  for (let [key,value] of Object.entries(loc[location])){
+    const row = widget.addStack()
+    let berry = row.addText(value.berry)
+    berry.textColor = Color.white()
+    berry.font = Font.boldMonospacedSystemFont(10)
+    row.addSpacer()
+    let image = row.addText(statusImage(value.status))
+    image.font = Font.boldMonospacedSystemFont(10)
+  }
 }
